cbfc7c397499a8d191b6e10a607bb8225c212ed6
bear <xxrBear@gmail.com>
2023-03-06 11:30:52 +0800

    用户注册接口增加setUpTestData

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -5,15 +5,20 @@ from apps.user.models import User
 
 class UserTest(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        """设置测试数据"""
+        User.objects.create(nickname='吴彦祖')
+        cls.client = Client()
+
     def test_register_user(self):
-        client = Client()
-        response = client.post('/register/', data={'nickname': 'jack chen'})
+        """测试注册接口"""
+        response = self.client.post('/register/', data={'nickname': 'jack chen'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(str(response.content, 'utf8'), '注册成功')
 
     def test_namesake_user(self):
-        client = Client()
-        User.objects.create(nickname='吴彦祖')
-        response = client.post('/register/', data={'nickname': '吴彦祖'})
+        """测试同名用户注册"""
+        response = self.client.post('/register/', data={'nickname': '吴彦祖'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(str(response.content, 'utf8'), '注册失败')
