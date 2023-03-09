79b9aa7abaa71fddd84f035cd2e762f9dcf35df5
bear <xxrBear@gmail.com>
2023-03-09 11:04:44 +0800

    修改user的view

--- apps/user/views.py
+++ apps/user/views.py
@@ -9,20 +9,17 @@ from apps.user.models import User
 class LoginView(View):
     """登录视图"""
 
-    @staticmethod
-    def get(request):
+    def get(self, request):
         return render(request, template_name='login.html')
 
 
 class RegisterView(View):
     """注册视图"""
 
-    @staticmethod
-    def get(request):
+    def get(self, request):
         return render(request, template_name='register.html')
 
-    @staticmethod
-    def post(request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             User.objects.create(**form.cleaned_data)
