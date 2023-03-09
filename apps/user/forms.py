8cc4d62ead8a062d6bc72b825f3d4c040fe1a7c5
bear <xxrBear@gmail.com>
2023-03-09 13:02:21 +0800

    设置user模块的form表单

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,12 +1,12 @@
 from django import forms
+from django.contrib.auth.hashers import make_password
 
 from apps.user.models import User
 
 
-class UserRegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = '__all__'
+class UserRegisterForm(forms.Form):
+    nickname = forms.CharField()
+    password = forms.CharField()
 
     def clean_nickname(self):
         nickname = self.cleaned_data['nickname']
@@ -15,3 +15,11 @@ class UserRegisterForm(forms.ModelForm):
             raise forms.ValidationError('昵称: {}已存在!'.format(nickname))
         else:
             return nickname
+
+    def clean_password(self):
+        p1 = self.data.get('password')
+        p2 = self.data.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError('两次密码不一致!')
+        else:
+            return make_password(p1)
